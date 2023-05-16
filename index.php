1e22a68b92958213f5636d6b35f4b8b0dd341788
RestyNakisitu <119209526+RestyNakisitu@users.noreply.github.com>
2023-05-16 10:38:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" > -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" ></script> -->
+</head>
+<body>
+    <div class="header">
+        <h1>Follow Us</h1>
+
+        <div class="social-links">
+               <a href="#"> <i class="fa fa-map-marker"></i></i>Kihihi</a>
+               <a href="#"><i class="fa fa-envelope"></i>info@example.com</a>
+               <a href="#"><i class="fa fa-phone"></i>+555 555 5555</a>
+          
+        </div>
+
+
+        <div class="social-media">
+            <i class="fa fa-facebook"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-instagram"></i>
+            <i class="fa fa-linkedin"></i>
+        </div>
+
+
+    </div>
+
+    <div class="navbar" id="navbar">
+
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Projects</a></li>
+        <!-- <li class="dropdown"><a href="#">Pages</a><i class="fa fa-caret-down"></i>
+            <div class="dropdown-menu">
+                <a href="#">What we do</a>
+                <a href="#">Services</a>
+                <a href="#">Testimonial</a>
+                <a href="#">Contact</a>
+            </div>
+        </li> -->
+        
+       <li><a href="#">Contact</a></li>
+    </ul>
+
+    <div class="search">
+
+            <input type="text" name="search" placeholder="search...">
+            <button type="submit"><i class="fa fa-search"></i></button>
+    
+        
+    </div>
+        
+
+    </div>
+
+
+    <div class="display">
+        <div class="display-text">
+
+            <h2>My Web <br> Application</h2>
+            <p>Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit, sed do eiusmod tempor incididunt <br> ut labore et dolore magna aliqua.</p>
+        
+            <button class="btn">Learn More</button>
+
+        </div>
+
+    </div>
+
+
+    <div class="card">
+        <h1>What we offer</h1>
+
+        <div class="card-div">
+            <div class="card-divs">
+                <img src="icon-1.png">
+                <h3>Web design</h3>
+                <p> Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit. </p>
+            </div>
+
+            <div class="card-divs">
+                <img src="icon-2.png">
+                <h3>App Development</h3>
+                <p class="role">Neque molestias modi ut quam? <br> Perferendis ullam </p>
+            </div>
+
+            <div class="card-divs">
+                <img src="icon-3.png">
+                <h3>UX design</h3>
+                <p>Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit. </p>
+            </div>
+
+            <div class="card-divs">
+                <img src="icon-4.png">
+                <h3>Testing</h3>
+                <p> Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit. </p>
+            </div>
+
+        </div>
+    </div>
+
+   <div class="about">
+    <div class="about-left">
+        <h1>what we are</h1>
+        <h2>debitis consequatur qui tempore illum iste adipisci, ex enim eveniet recusandae architecto, delectus accusamus nobis eos doloremque.</h2>
+        <p>dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <button class="btn">Read More <i class="fa fa-arrow-right"></i></button>
+
+    </div>
+
+    <div class="about-right">
+        <img src="m1.jpg">
+    </div>
+
+   </div>
+
+
+ <div class="values">
+ <h1>Services</h1>
+
+      <div class="value">
+      
+
+        <div class="value-div">
+            <img src="icon-5.png">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Lorem ipsum dolor sit amet,laudantium eius sunt illum consequuntur dolore dolorum eum.</p>
+        </div>
+      
+
+
+     
+        <div class="value-div">
+            <img src="icon-6.png">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Lorem ipsum dolor sit amet,laudantium eius sunt illum consequuntur dolore dolorum eum.</p>
+        </div>
+    
+
+    
+        <div class="value-div">
+            <img src="icon-7.png">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Lorem ipsum dolor sit amet,laudantium eius sunt illum consequuntur dolore dolorum eum.</p>
+        </div>
+
+        <div class="value-div">
+            <img src="icon-8.png">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Lorem ipsum dolor sit amet,laudantium eius sunt illum consequuntur dolore dolorum eum.</p>
+        </div>
+
+        <div class="value-div">
+            <img src="icon-9.png">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Lorem ipsum dolor sit amet,laudantium eius sunt illum consequuntur dolore dolorum eum.</p>
+        </div>
+
+        <div class="value-div">
+            <img src="icon-10.png">
+            <h3>Ad cupiditate sed est odio</h3>
+            <p>Lorem ipsum dolor sit amet,laudantium eius sunt illum consequuntur dolore dolorum eum.</p>
+        </div>
+        
+      </div> 
+   </div> 
+
+
+
+   <div class="projects">
+    <h1>Our Projects</h1>
+    <div class="projects-div">
+        <div>
+            <img src="img2.jpg">
+        </div>
+        <div>
+            <img src="img3.jpg">
+        </div>
+        <div>
+            <img src="img1.jpg">
+        </div>
+        <div>
+            <img src="img3.jpg">
+        </div>
+        <div>
+            <img src="img7.jpg">
+        </div>
+        <div>
+            <img src="img8.jpg">
+        </div>
+
+    </div>
+   </div>
+
+
+   <div class="contact">
+      <h1>Contact Us</h1>
+
+      <div class="contactus">
+        <div class="contact-left">
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <h4>Location</h4>
+                <p>Kihihi tyddb</p>
+               
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <h4>Email</h4>
+                <p>info@example.com</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <h4>Contact</h4>
+                <p>+555 555 5555</p> 
+            </div>
+               
+        </div>
+
+        <div class="contact-right">
+
+            <form action="">
+                <input type="text" name="name" placeholder="name">
+                <input type="text" name="email" placeholder="email">
+                <input type="text" name="subject" placeholder="subject">
+                <textarea name="message" id="" cols="30" rows="10" placeholder="message"></textarea>
+           
+                <button class="btn">Submit</button>
+            </form>
+
+        </div>
+      </div>
+   </div>
+
+   <div class="footer">
+    <div class="footer-div">
+        
+        <div>
+            <h4>Usefull Links</h4>
+             <ul>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">Services</a>
+                </li>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h4>Our Services</h4>
+             <ul>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">Web dessign</a>
+                </li>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">App Development</a>
+                </li>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">UX Design</a>
+                </li>
+                <li>
+                    <i class="fa fa-chevron-right"></i>
+                    <a href="">Testing</a>
+                </li>
+            
+            </ul>
+        </div>
+
+        <div>
+            <h4>social Media links</h4>
+
+            <ul>
+                <li><a href="#"> <i class="fa fa-facebook"></i></i>Facebook</a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i>twitter</a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i>instagram</a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i>linkedin</a></li>
+            </ul>
+
+            <!-- <i class="fa fa-facebook"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-instagram"></i>
+            <i class="fa fa-linkedin"></i> -->
+
+        </div>
+
+        <div>
+            <h4>Location</h4>
+
+            <ul>
+                <li><a href="#"> <i class="fa fa-map-marker"></i>Kihihi</a></li>
+                <li><a href="#"><i class="fa fa-envelope"></i>info@gmail.com</a></li>
+                <li><a href="#"><i class="fa fa-phone"></i>+1 555 555 5555</a></li>
+            </ul>
+
+
+        </div>
+
+        <div>
+            <h4>Our news letter</h4>
+            <p>Lorem ipsum dolor sit Doloremque fugit illum distinctio culpa odio!</p>
+            <form action="" method="post">
+              <input type="email" name="email">
+              <input type="submit" value="Subscribe">
+            </form>
+        </div>
+
+
+    </div>
+    <p>All rights reserved | Developed by Nakisitu Resty</p>
+   </div>
+
+
+    <script>
+        window.onscroll = function(){
+            myFunction();
+        }
+
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+        // console.log(navbar.offsetTop);
+
+        function myFunction(){
+            if(window.pageYOffset >= sticky){
+                navbar.classList.add("sticky");
+                console.log(navbar.offsetTop);
+            }else{
+                navbar.classList.remove("sticky");
+            }
+        }
+    </script>
+</body>
+</html>
+
